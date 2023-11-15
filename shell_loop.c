c8ac7cc68b771b7b44fd88d3afdf74e4fcd48bb5
MUGIRANEZA PROTOGENE <inezaprotogene@gmail.com>
2023-11-15 10:40:05 +0200

    update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -31,5 +31,73 @@ int hsh(info_t *, char **av)
 				exit(info->status);
 			exit(info->err_num);
 		}
+		return (builtin_ret);
 	}
 }
+/**
+ * find_builtin - finds a buitin command
+ * @infoz: the parametet & return info struct
+ *
+ * Return: -1 if buitin not found,
+ *         0 If builtin found  but not successfull,
+ *         -2 if builtin signals exit()
+ */
+int find_builtin(info_t *info)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = 
+	{
+		{"exit", _myexit},
+		{"env", _myenv},
+		{"help", _myhelp},
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _mycd},
+		{"alias", _myalias},
+		{NULL, NULL}
+	};
+	for (i = 0; builtintbl[i].type; i++)
+	{
+		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			inf->line_count++;
+			built_in_ret = bultintbl[i].func(inf);
+			break;
+		}
+		return (built_in_ret);
+	}
+}
+
+/**
+ * fnd_cmd - finds a commands in path
+ * @info: the parameter & return info struct
+ *
+ * Return: void
+ */
+void find_cmd(info_t *t)
+{
+
+char *path = NULL;
+int i,k;
+
+info->path = NULL;
+if (info->linecount_flag == 1)
+{
+	info->line_count++;
+	info->linecount_flag = 0;
+}
+for (i = 0, k = 0; info->arg[i]; i++)
+if (!is_delim(info->[i], "\t\n"))
+k++;
+if (!k)
+	return;
+
+path = find_path(info,_getenv(info,"PATH"),  info->argv[0]);
+if(path)
+{
+	inf->path = path
+	fork
+}
+}
+
